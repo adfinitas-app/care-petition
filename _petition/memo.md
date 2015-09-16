5c089bb579c9ea1f12636bf0cb5673d30fe93fba
adfinitas42 <adfinitas42@dhcp118.adfinitas.fr>
2015-09-16 17:48:15 +0200

    petition

---
+++ _petition/memo.md
@@ -0,0 +1,43 @@
+title: (url)
+contenu_petition : 
+  header:
+    titre:
+    introduction: 
+    image_path:
+  formulaire:
+    introduction_formulaire:
+    phrase_telephone:
+    config_form:
+      nb_signataires:
+      civility:
+        visibility: false
+        required: false
+      firstname:
+        visibility: true
+        required: false
+      lastname:
+        visibility: true
+        required: false
+      address1:
+        visibility: false
+        required: false
+      address2:
+        visibility: false
+      postcode:
+        visibility: false
+        required: false
+      city:
+        visibility: false
+        required: false
+     phone:
+        visibility: true
+        required: true
+     content:
+        titre:
+  footer:
+    titre:
+    introduction:
+    contenu:
+      left:
+      mid:
+      right:
---
+++ backup/static/js/custom.js
@@ -0,0 +1,77 @@
+function scrollToEl(el) {
+	$('html, body').animate({
+		scrollTop: $(el).offset().top
+	});
+}
+
+$(document).ready(function() {
+	if(screen.width > 1400) {
+		$('.bg1').height(screen.width*535/1400);
+		$('.bg2').height(screen.width*200/1400);
+		$('.bg3').height(screen.width*463/1400);
+		$('.bg4').height(screen.width*535/1400);
+	}
+	
+	$(window).on('resize', function() {
+		if(screen.width > 1400) {
+			$('.bg1').height(screen.width*535/1400);
+			$('.bg2').height(screen.width*200/1400);
+			$('.bg3').height(screen.width*463/1400);
+			$('.bg4').height(screen.width*535/1400);
+		}
+		else {
+			$('.bg1, .bg2, .bg3, .bg4').css('height', 'auto');
+		}
+	})
+
+  var phoneInput = $("#form-phone"),
+    phoneError = $("#phone-error"),
+    phoneOk = $("#phone-ok");
+
+  phoneInput.intlTelInput({
+    autoPlaceholder: false,
+    utilsScript: "lib/js/vendor/utils.js",
+    defaultCountry: "auto",
+      geoIpLookup: function(callback) {
+        $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+          var countryCode = (resp && resp.country) ? resp.country : "";
+          callback(countryCode);
+        });
+      }
+  });
+  phoneInput.blur(function() {
+    if ($.trim(phoneInput.val())) {
+      if (phoneInput.intlTelInput("isValidNumber")) {
+        phoneOk.removeClass("hide");
+      } else {
+        phoneInput.addClass("error");
+        phoneError.removeClass("hide");
+        phoneOk.addClass("hide");
+      }
+    }
+  });
+  phoneInput.keydown(function() {
+    phoneInput.removeClass("error");
+    phoneError.addClass("hide");
+    phoneOk.addClass("hide");
+  });
+
+
+  jQuery.validator.addMethod('validatePhone', function () {
+    if ($("#form-phone").intlTelInput("isValidNumber")) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }, "Numéro incorrect");
+
+  $('form.custom').validate({
+    rules: {
+      phone:{
+          required: true,
+          validatePhone: true
+      }
+    }
+  });
+});
